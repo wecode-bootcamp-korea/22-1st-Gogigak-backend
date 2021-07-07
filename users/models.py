b3c1221cb633c3d1dbfca9c06d979a3ef6ee4948
hyeonjung choi <waithjno@gmail.com>
2021-07-07 09:57:48 +0900

    model 수정

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    email        = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100, unique=True)
     password     = models.CharField(max_length=200)
     name         = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, unique=True)
     point        = models.IntegerField(default=0)
     coupons      = models.ManyToManyField('Coupon', through='UserCoupon')
 
