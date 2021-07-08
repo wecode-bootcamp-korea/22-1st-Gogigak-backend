8bdc612c9b275802f7c9409605ab5257c9aa3169
서정민 <sayho2021@gmail.com>
2021-07-09 01:10:27 +0900

    Revise: error handling and urls

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls    import path
 from products.views import ProductView
 
 urlpatterns = [
-    path('product/<int:product_id>', ProductView.as_view()),
+    path('/<int:product_id>', ProductView.as_view()),
 ]
