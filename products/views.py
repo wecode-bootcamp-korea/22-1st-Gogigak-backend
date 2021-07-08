98d5947bd738afd600dd1a57e0c5c1e560e4652a
서정민 <sayho2021@gmail.com>
2021-07-08 17:30:52 +0900

    Add: error handling in ProductView

--- products/views.py
+++ products/views.py
@@ -4,24 +4,27 @@ from django.views import View
 from products.models import Product
 
 class ProductView(View):
-    def get(self, request):
-        # product = Product.objects.get(id=product_id)
-        product = Product.objects.get(id=request.GET['id'])
-        print(product)
-        price_per_100g = str(int(product.price) / int(product.grams) * 100)
-        options = product.options.all()
-        images = product.image_set.all()
+    def get(self, request, product_id):
+        try: 
+            product        = Product.objects.get(id=product_id)
 
-        results = {
-            'name': product.name,
-            'butcheredDate': product.butchered_date,
-            'price': product.price,
-            'grams': product.grams,
-            'pricePer100g': price_per_100g,
-            'isOrganic': product.is_organic,
-            'thumbnail': product.thumbnail,
-            'options': [option.name for option in options],
-            'images': [{'imageUrl': image.image_url, 'sequence': image.sequence} for image in images]
-        }
+            price_per_100g = str(round((int(product.price) / int(product.grams) * 100), 2))
+            options        = product.options.all()
+            images         = product.image_set.all()
+        
+            results = {
+                'name'         : product.name,
+                'butcheredDate': product.butchered_date,
+                'price'        : product.price,
+                'grams'        : product.grams,
+                'pricePer100g' : price_per_100g,
+                'isOrganic'    : product.is_organic,
+                'thumbnail'    : product.thumbnail,
+                'options'      : [option.name for option in options],
+                'images'       : [{'imageUrl': image.image_url, 'sequence': image.sequence} for image in images]
+            }
 
-        return JsonResponse({'results': results}, status=200)
+            return JsonResponse({'results': results}, status=200)
+            
+        except Product.DoesNotExist:
+            return JsonResponse({'message': 'PRODUCT_NOT_FOUND'}, status=400)
