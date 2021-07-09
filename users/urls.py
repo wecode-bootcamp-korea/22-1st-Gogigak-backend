12b2c25bc0986c4b1ac8d601ae17b1ce829b5740
hyeonjung choi <waithjno@gmail.com>
2021-07-09 16:16:25 +0900

    Add: mypage view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, SignInView
+from .views import SignUpView, SignInView , MyPageView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
+    path('/mypage', MyPageView.as_view()),
 ]
