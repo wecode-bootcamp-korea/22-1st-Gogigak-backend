75f67afd0544d2b2cb59fd62de5a5d384f851f11
서정민 <sayho2021@gmail.com>
2021-07-09 17:34:36 +0900

    Add: BestItemView

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views    import View
+from django.http     import JsonResponse
 
-# Create your views here.
+from products.models import Product
+
+class BestItemView(View):
+    def get(self, request):
+        products = Product.objects.all().order_by('-sales')[:6]
+        results = []
+       
+        for product in products:
+            results.append(
+                {
+                    'id'       : product.id,
+                    'name'     : product.name, 
+                    'price'    : int(product.price),
+                    'grams'    : int(product.grams),
+                    'thumbnail': product.thumbnail,
+                    'isOrganic': product.is_organic
+                }
+            )
+        
+        return JsonResponse({'results': results}, status=200)
