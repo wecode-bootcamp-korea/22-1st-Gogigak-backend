06482cec0f04a6a977c98da5281ec4be5cf67b78
Junyoung Lee <ambitiouskyle@gmail.com>
2021-07-09 16:54:47 +0900

    ADD: cart-items delete function

--- orders/views.py
+++ orders/views.py
@@ -28,3 +28,23 @@ class CartView(View):
         
         except User.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+    @login_decorator
+    def delete(self, request, cart_item):
+        try:
+            signed_user = request.user
+
+            if cart_item == 0:
+                items = CartItem.objects.filter(user=signed_user)
+                for item in items:
+                    item.delete()
+                return JsonResponse({'message':'DELETE_SUCCESS'}, status=204)
+
+            if not CartItem.objects.filter(pk=cart_item, user=signed_user).exists():
+                return JsonResponse({'message':'INVALID_ITEM'}, status=400)
+
+            CartItem.objects.get(pk=cart_item, user=signed_user).delete()
+            return JsonResponse({'message':'DELETE_SUCCESS'}, status=204)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=200)
