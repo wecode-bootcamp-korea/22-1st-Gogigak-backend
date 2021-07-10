308e678a7fd9fffbf058e0605585df3db57f0f03
Junyoung Lee <ambitiouskyle@gmail.com>
2021-07-10 15:09:55 +0900

    ADD: Quantity change function

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ import json
 from django.views         import View
 from django.http.response import JsonResponse
 
-from users.models    import User, Product
+from users.models    import User
 from orders.models   import CartItem
 
 class CartView(View):
@@ -34,12 +34,25 @@ class CartView(View):
     @login_decorator
     def patch(self, request, cart_item):
         try:
-            data = json.loads(request.body)
-            signed_user = request.user
-            is_add = data['isAdd']
+            data            = json.loads(request.body)
+            signed_user     = request.user
+            change_quantity = data['changeQuantity']
+
             if not CartItem.objects.filter(pk=cart_item, user=signed_user).exists():
                 return JsonResponse({'message':'INVALID_ITEM'}, status=400)
             
-            item = CartItem.objects.get(pk=cart_item)
-            if is_add:
-                
+            item    = CartItem.objects.get(pk=cart_item)
+            product = item.product_options.product
+
+            if (item.quantity + change_quantity) > product.stock:
+                return JsonResponse({'message':'OUT_OF_STOCK'}, status=400)
+            
+            if (item.quantity + change_quantity) < 1:
+                return JsonResponse({'message':'INVALID_QUANTITY'}, status=400)
+            
+            item.quantity += change_quantity
+            item.save()
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
