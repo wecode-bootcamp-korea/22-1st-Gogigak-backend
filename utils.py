18a1a90093bcc85826c4bd7dff4bf18bbcac0a49
hyeonjung choi <waithjno@gmail.com>
2021-07-12 17:51:16 +0900

    컨벤션 수정

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ from my_settings  import SECRET_KEY
 def login_decorator(func):
     def wrapper(self, requst, *args, **kwargs):
         try:
-            token = requst.headers.get("Authorization", None)
-            payload = jwt.decode(token, SECRET_KEY, algorithms="HS256")
-            user = User.objects.get(id = payload['user_id'])
+            token       = requst.headers.get("Authorization", None)
+            payload     = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+            user        = User.objects.get(id = payload['user_id'])
             requst.user = user
         
         except jwt.exceptions.DecodeError:
