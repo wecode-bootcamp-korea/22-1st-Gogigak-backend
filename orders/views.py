6fde24f907d6cb96d2df3e776cc98f3ea867cb0d
Junyoung Lee <ambitiouskyle@gmail.com>
2021-07-13 09:40:05 +0900

    Logic 수정

--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,7 @@ from users.models    import User
 from orders.models   import CartItem
 
 class CartView(View):
-    @login_decorator
+    # @login_decorator
     def get(self, request):
         try:
             signed_user = request.user
@@ -31,26 +31,27 @@ class CartView(View):
         except User.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=400)
 
-    @login_decorator
+    # @login_decorator
     def patch(self, request, cart_item):
         try:
             data            = json.loads(request.body)
-            signed_user     = request.user
+            signed_user     = User.objects.get(pk=1)#request.user
             change_quantity = data['changeQuantity']
 
             if not CartItem.objects.filter(pk=cart_item, user=signed_user).exists():
-                return JsonResponse({'message':'INVALID_ITEM'}, status=400)
-            
-            item    = CartItem.objects.get(pk=cart_item)
-            product = item.product_options.product
+                return JsonResponse({'message':'NOT_FOUND'}, status=404)
 
-            if (item.quantity + change_quantity) > product.stock:
+            item           = CartItem.objects.get(pk=cart_item)
+            product        = item.product_options.product
+            total_quantity = item.quantity + change_quantity
+
+            if total_quantity < 1:
+                total_quantity = 1
+
+            if total_quantity > product.stock:
                 return JsonResponse({'message':'OUT_OF_STOCK'}, status=400)
-            
-            if (item.quantity + change_quantity) < 1:
-                return JsonResponse({'message':'INVALID_QUANTITY'}, status=400)
-            
-            item.quantity += change_quantity
+
+            item.quantity = total_quantity
             item.save()
             return JsonResponse({'message':'SUCCESS'}, status=200)
 
