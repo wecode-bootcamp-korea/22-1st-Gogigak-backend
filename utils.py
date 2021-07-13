4992cc24cdcb40df558b42ae50855a33d7b7961d
hyeonjung choi <waithjno@gmail.com>
2021-07-13 10:53:06 +0900

    decorator 오타 수정

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ from users.models import User
 from my_settings  import SECRET_KEY
 
 def login_decorator(func):
-    def wrapper(self, requst, *args, **kwargs):
+    def wrapper(self, request, *args, **kwargs):
         try:
-            token       = requst.headers.get("Authorization", None)
-            payload     = jwt.decode(token, SECRET_KEY, algorithms="HS256")
-            user        = User.objects.get(id = payload['user_id'])
-            requst.user = user
+            token        = request.headers.get("Authorization", None)
+            payload      = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+            user         = User.objects.get(id = payload['user_id'])
+            request.user = user
             
-            return func(self, requst, *args, **kwargs)
+            return func(self, request, *args, **kwargs)
         
         except jwt.exceptions.DecodeError:     
             return JsonResponse({'message' : 'INVALID TOKEN'}, status = 400)         
